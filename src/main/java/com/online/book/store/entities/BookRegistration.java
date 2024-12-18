80f28834cc944f3b83e6fd90f51f450efb50f46c
armht <93738626+ANANIMUS1337-mantoed@users.noreply.github.com>
2024-12-18 23:12:00 +0500

    Initial commit

---
+++ src/main/java/com/online/book/store/entities/BookRegistration.java
@@ -0,0 +1,62 @@
+package com.online.book.store.entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+
+@Entity
+public class BookRegistration {
+
+	@Id
+	String Book_title;
+	String Author;
+	String Reviews;
+	String rate;
+	String price;
+	public String getPrice() {
+		return price;
+	}
+	public void setPrice(String price) {
+		this.price = price;
+	}
+	public String getBook_title() {
+		return Book_title;
+	}
+	public void setBook_title(String book_title) {
+		Book_title = book_title;
+	}
+	public String getAuthor() {
+		return Author;
+	}
+	public void setAuthor(String author) {
+		Author= author;
+	}
+	public String getReviews() {
+		return Reviews;
+	}
+	public void setReviews(String reviews) {
+		Reviews = reviews;
+	}
+	public String getRate() {
+		return rate;
+	}
+	public void setRate(String rate) {
+		this.rate = rate;
+	}
+	boolean isHomePage;
+
+	// Геттеры и сеттеры
+	public boolean isHomePage() {
+		return isHomePage;
+	}
+
+	public void setHomePage(boolean homePage) {
+		isHomePage = homePage;
+	}
+
+	public Object getImagePath() {
+		return null;
+	}
+
+	public void setImagePath(String s) {
+	}
+}
---
+++ src/main/java/com/online/book/store/repositories/BookRepository.java
@@ -0,0 +1,19 @@
+package com.online.book.store.repositories;
+
+import com.online.book.store.entities.BookRegistration;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.repository.CrudRepository;
+
+import java.util.List;
+
+public interface BookRepository extends CrudRepository<BookRegistration, String> {
+    static List<BookRegistration> findByIsHomePage(boolean b) {
+        return null;
+    }
+
+    List<BookRegistration> findAll();
+    Page<BookRegistration> findAll(Pageable pageable);
+
+
+}
